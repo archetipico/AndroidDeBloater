4078cd361c3f717c8f6687d11176fe2d848dd1cd
Edoardo Ferrari <43927507+archetipico@users.noreply.github.com>
2022-05-06 00:44:58 +0200

    Update

--- bloat.sh
+++ bloat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat bloaters | awk '{print $1; system("adb shell cmd package install-existing " $1)}'
+cat bloaters | awk '{printf "\n%s\n",$1; system("adb shell cmd package install-existing " $1)}'
--- debloat.sh
+++ debloat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat bloaters | awk '{print $1; system("adb shell pm uninstall -k --user 0 " $1)}'
+cat bloaters | awk '{printf "\n%s\n",$1; system("adb shell pm uninstall -k --user 0 " $1)}'
